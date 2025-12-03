343d7176c4287aab7f8343d05eaebecbed19b71a
alex-fdr <alexfv01@gmail.com>
2025-12-03 22:12:47 +0200

    update physics component

--- src/physics/physics.ts
+++ src/physics/physics.ts
@@ -2,21 +2,17 @@ import { NaiveBroadphase, World } from 'cannon-es';
 import type { Vector3Like } from 'three';
 
 export type PhysicsProps = {
-    gravity: Vector3Like;
+    gravity?: Vector3Like;
 };
 
 export class Physics {
-    timeStep: number;
-    lastCallTime: number;
-    maxSubSteps: number;
+    timeStep = 1 / 60;
+    lastCallTime = 0;
+    maxSubSteps = 3;
     world: World;
 
     constructor(config: PhysicsProps) {
-        const { gravity } = config;
-
-        this.timeStep = 1 / 60;
-        this.lastCallTime = 0;
-        this.maxSubSteps = 3;
+        const { gravity = { x: 0, y: 0, z: 0 } } = config;
 
         this.world = new World();
         this.world.broadphase = new NaiveBroadphase();
