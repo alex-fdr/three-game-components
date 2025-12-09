34685edbd018ae3db5a743f901d473f0476a7351
alex-fdr <alexfv01@gmail.com>
2025-12-09 23:42:07 +0200

    change build output format

--- vite.config.js
+++ vite.config.js
@@ -6,34 +6,25 @@ export default defineConfig({
     build: {
         outDir: './dist',
         lib: {
-            entry: {
-                physics: resolve(__dirname, './src/physics/index.ts'),
-                animations: resolve(__dirname, './src/animations/index.ts'),
-                tweens: resolve(__dirname, './src/tweens/index.ts'),
-            },
+            entry: resolve(__dirname, './src/index.ts'),
             formats: ['es'],
-            
+
         },
         rollupOptions: {
             external: [
-                'three', 
-                'cannon-es', 
-                'three/addons/utils/SkeletonUtils', 
+                'three',
+                'cannon-es',
+                'three/addons/utils/SkeletonUtils',
                 '@alexfdr/three-game-core',
                 '@tweenjs/tween.js'
-            ],
-            output: {
-                entryFileNames: ({name}) => `${name}/[name].js`
-            }
+            ]
         },
         reportCompressedSize: false,
     },
     plugins: [
-        dts({ 
-            exclude: ['node_modules/**', '**/index.ts'],
-            outDir: './dist/',
-            // rollupTypes: true, 
-            // insertTypesEntry: true, 
+        dts({
+            exclude: ['node_modules/**'],
+            rollupTypes: true,
         })
     ]
-})
+});
