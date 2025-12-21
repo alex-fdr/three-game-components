fe74259b3b008595c50c90f3ad2a6b5005ade275
alex-fdr <alexfv01@gmail.com>
2025-12-21 22:30:30 +0200

    re-export input handlers from the input component

--- src/input/index.ts
+++ src/input/index.ts
@@ -1,5 +1,6 @@
 import { InputSystem } from './input';
 
+export { DragHandler } from './handlers/drag';
 export type { InputHandler, InputStatus } from './input';
 
 export const input = new InputSystem();
